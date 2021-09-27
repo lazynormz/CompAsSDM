7e63467d3e9f31ef4290538d952253e5c4d982e7
EmilseBilse <emil875b@easv365.dk>
2021-09-27 10:17:17 +0200

    irepo

---
+++ MovieRating.Domain/IRepositories/IReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MovieRating.Core.Model;
+
+namespace MovieRating.Domain.IRepositories
+{
+    public interface IReviewRepository
+    {
+        List<MovieReview> FindAll();
+    }
+}
